8e94916558c7909085d3cb218e397faea6b39545
Facundo Victor <facundovt@gmail.com>
2018-06-18 09:08:19 -0300

    OAS-11: Update script to get all CF files within the repo

--- ci.sh
+++ ci.sh
@@ -8,3 +8,6 @@ IFS=$'\n\t'
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 # http://stackoverflow.com/a/246128/424301
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Search for all CF/YML/YAML/JSON files within the repo
+CF_TEMPLATES=$(find ${DIR}/* -type f \( -iname '*.cf' -o -iname '*.yml' -o -iname '*.yaml' -o -iname '*.json' \))
