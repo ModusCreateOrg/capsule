3f1e05243407cbd4cecba87641416c8f5e8fea53
Facundo Victor <facundovt@gmail.com>
2018-06-25 14:46:37 -0300

    WIP: [OAS-11]: add scripts for template validation (#6)
    * OAS-11: Add base script
    
    * OAS-11: Update script to get all CF files within the repo
    
    * OAS-11: Add validate-template call to all templates

---
+++ ci.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+# Credit to Stack Overflow questioner Jiarro and answerer Dave Dopson
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+# http://stackoverflow.com/a/246128/424301
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Search for all CF/YML/YAML/JSON files within the repo
+CF_TEMPLATES=$(find ${DIR}/* -type f \( -iname '*.cf' -o -iname '*.yml' -o -iname '*.yaml' -o -iname '*.json' \))
+
+for i in $CF_TEMPLATES ; do
+    echo "Testing: $i"; \
+    aws cloudformation validate-template --template-body file://$(realpath $i) > /dev/null ;
+done
