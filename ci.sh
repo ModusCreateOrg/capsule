c0286300e86cb64c322ce80585d38a2a3783ffc0
Facundo Victor <facundovt@gmail.com>
2018-07-30 18:56:53 -0300

    [OAS-14]: fix ci script (#14)
    * [OAS-14]: Include all variants of CF yaml files
    
    * [OAS-14]: Include all CF json formats
    
    * OAS-14: Empty commit to test CI

--- ci.sh
+++ ci.sh
@@ -9,10 +9,29 @@ IFS=$'\n\t'
 # http://stackoverflow.com/a/246128/424301
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Returns all variants of CF templates
+function search_cf_files () {
+    find "${DIR}"/* \
+        -type f \
+        \(  \
+            -iname '*.cf' \
+            -o \
+            -iname 'template.*.yaml' \
+            -o \
+            -iname '*.cf.yaml' \
+            -o \
+            -iname 'template.*.json' \
+            -o \
+            -iname '*.cf.json' \
+        \)
+}
+
 # Search for all CF files within the repo
-CF_TEMPLATES=$(find ${DIR}/* -type f  -name '*.cf' )
+CF_TEMPLATES=$(search_cf_files)
 
 for i in $CF_TEMPLATES ; do
-    echo "Testing: $i"; \
-        aws cloudformation validate-template --template-body "file://$i" > /dev/null ;
+    echo "Testing: $i";
+    aws cloudformation validate-template \
+        --template-body "file://$i" \
+        > /dev/null ;
 done
